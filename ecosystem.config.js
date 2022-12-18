500dd8ffef92452bc38d54bfc2aedea0186078b0
anas sawalha <anas.swalha@yahoo.com>
2022-12-18 08:45:24 +0200

    updated pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "api.event.notifier.agentsoncloud.com_Release_3",
       script: "./bin/www",
-      instances: "1",
-      exec_mode: "fork",
+      instances: "6",
+      exec_mode: "cluster",
     },
   ],
 };
