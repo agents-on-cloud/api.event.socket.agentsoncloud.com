d9a160b7c243b687011cfd0df546419b1e81de83
saleh ziad <87468273+Salehziad@users.noreply.github.com>
2023-02-13 16:35:34 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "api.event.notifier.agentsoncloud.com_Release_3",
       script: "./bin/www",
-      instances: "1",
+      instances: "8",
       exec_mode: "cluster",
     },
   ],
